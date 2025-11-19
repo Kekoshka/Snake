6fda9e22ae05d82fe3f096785e331f881aebcf55
student-a502 <student-a502@STUD-RM5-A502>
2025-11-19 17:11:47 +0500

    Чота сделано

--- Services/SnakeDriverService.cs
+++ Services/SnakeDriverService.cs
@@ -1,6 +1,8 @@
 ﻿
 using Microsoft.Extensions.Caching.Memory;
+using Snake.Enums;
 using Snake.Extensions;
+using Snake.Models;
 
 namespace Snake.Services
 {
@@ -16,16 +18,41 @@ namespace Snake.Services
         public Task StartAsync(CancellationToken cancellationToken)
         {
             _timer = new Timer(UpdateSnakePlace, null, 0, 50);
+            return Task.CompletedTask;
         }
 
-        private void UpdateSnakePlace()
+        private void UpdateSnakePlace(object? state)
         {
             var snakes = _cache.GetKeyValueItems<Models.Snake>(key => key.StartsWith("S_"));
+            snakes.ForEach(snake =>
+            {
+                ChangeBodyPlace(snake.Value);
+                ChangeHeadPlace(snake.Value);
+            }
+            );
+        }
+
+
+        private void ChangeHeadPlace(Models.Snake snake)
+        {
+            var oldSnakeHeadPlace = snake.SnakePositions.Single(sp => sp.Order == 1);
+            var newSnakeHeadPlace = snake.Orientation switch
+            {
+                1 => new SnakePosition { Order = 0, X = oldSnakeHeadPlace.X, Y = oldSnakeHeadPlace.Y + 1 },
+                2 => new SnakePosition { Order = 0, X = oldSnakeHeadPlace.X + 1, Y = oldSnakeHeadPlace.Y },
+                3 => new SnakePosition { Order = 0, X = oldSnakeHeadPlace.X, Y = oldSnakeHeadPlace.Y - 1 },
+                4 => new SnakePosition { Order = 0, X = oldSnakeHeadPlace.X - 1, Y = oldSnakeHeadPlace.Y }
+            };
+        }
+        private void ChangeBodyPlace(Models.Snake snake)
+        {
+            snake.SnakePositions.Remove(snake.SnakePositions.OrderByDescending(sp => sp.Order).First());
+            snake.SnakePositions.ToList().ForEach(sp => sp.Order += 1);
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return Task.CompletedTask;
         }
     }
 }
